662b84b0183a72305a547b1011eca9240c99b2f2
afontele <afontele@f0r11s21.paris.42.school>
2026-05-28 23:51:24 +0200

    Amanda: FINISH, DOOOOONE, CIAO, JE VEUX PLUS

--- includes/CommandHandler.hpp
+++ includes/CommandHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: afontele <afontele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 15:55:06 by dnayel            #+#    #+#             */
-/*   Updated: 2026/05/28 23:46:27 by afontele         ###   ########.fr       */
+/*   Updated: 2026/05/28 23:49:03 by afontele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,23 @@ class CommandHandler
 		void		handleCommand(Server &server, Client *client, const Message &msg);
 
 	private:
-	// Nayel : Registration Commands
+	// Registration Commands
 		void		handlePASS(Server &server, Client *client, const Message &msg);
 		void		handleNICK(Server &server, Client *client, const Message &msg);
 		void		handleUSER(Server &server, Client *client, const Message &msg);
 		void		handlePING(Server &server, Client *client, const Message &msg);
 		void		handleQUIT(Server &server, Client *client, const Message &msg);
-
 		void		registerClient(Server &server, Client *client);
-
 		void		broadcastToChannel(Server &server, Channel *chan, const std::string &msg, Client *except);
-	//Aileen
-	//Fonctions de test (tu peux les utiliser aussi hein, c'est juste pour que tu voies ce que j'ai ajoute)
-		bool		isMemberChannel(Client *c, Channel *chan);//check dans client et chan si membre du chan
-		bool		checkChannelKey(Channel const *chan, std::string const key);//si chan n'a pas de clef, renvoie true sinon renvoie le resultat de isKey (comparaison avec la clef du chan)
-		bool		checkLimit(Channel const *chan);//true = limit channel non atteinte ou pas de limit, false = limite atteinte
-		Client		*checkClientExists(Server &server, std::string const &nickname);//verifier aussi que s'il existe il est bien register => NULL = client non existant/enregistre, sinon pointeur sur le client ?
+		bool		isMemberChannel(Client *c, Channel *chan);
+		bool		checkChannelKey(Channel const *chan, std::string const key);
+		bool		checkLimit(Channel const *chan);
+		Client		*checkClientExists(Server &server, std::string const &nickname);
 		bool		isValidChannelName(std::string const &name);
 		bool		isValidClientName(std::string const &name);
-		void		namesReply(Server &server, Client *c, Channel *chan);//A voir s'il faut le message du JOIN aussi pour Nayel
+		void		namesReply(Server &server, Client *c, Channel *chan);
 		std::string	mkModeList(Channel *chan);
-		//Commandes//Le client ici du coup c'est bien celui qui a appele la commande
+		//Commands
 		void		handleJOIN(Server &server, Client *c, const Message &msg);
 		void		handlePRIVMSG(Server &server, Client *c, const Message &msg);
 		void		handleKICK(Server &server, Client *c, const Message &msg);
@@ -57,7 +53,6 @@ class CommandHandler
 		void		handleTOPIC(Server &server, Client *c, const Message &msg);
 		void		handleMODE(Server &server, Client *c, const Message &msg);
 		void		handlePART(Server &server, Client *c, const Message &msg);
-		//void	handleNAMES(Server &server, Client *c, const Message &msg);//Non obg mais utile pour JOIN => j'ai plutot fait un helper, on peut passer par lui si finalement on decide de coder la commande NAMES
 };
 
 #endif
